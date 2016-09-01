0cdd5498cf1adaca465424b24bd22f3ddda0ccf3
nachomacaso <nathanmacaso@gmail.com>
2016-08-31 19:12:31 -0500

    Added Contact Form Param

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -6,4 +6,18 @@ class ContactsController < ApplicationController
   def all
     @contacts = Contact.all
   end
+
+  def add_form
+    @contact_first_name = params[:contact_first_name]
+    @contact_last_name = params[:contact_last_name]
+    @contact_email = params[:contact_email]
+    @contact_phone = params[:contact_phone]
+  end
+
+  def show_form
+    @contact_first_name = params[:contact_first_name]
+    @contact_last_name = params[:contact_last_name]
+    @contact_email = params[:contact_email]
+    @contact_phone = params[:contact_phone]
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get '/first' => 'contacts#first'
   get '/all' => 'contacts#all'
+  get '/contact_form' => 'contacts#add_form'
+  post '/contact_show' => 'contacts#show_form'
 end
